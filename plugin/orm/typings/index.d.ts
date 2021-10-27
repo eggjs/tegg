89835a94dccddeef9662afc784b513a4f16c115d
nightink <chceyes@gmail.com>
2021-10-27 14:50:52 +0800

    fix(orm): plugin types overrides ORM Lib Attribute

--- plugin/orm/typings/index.d.ts
+++ plugin/orm/typings/index.d.ts
@@ -1,2 +1,9 @@
 import 'egg';
 import '@eggjs/tegg-plugin';
+
+import { DataType } from 'leoric';
+import { AttributeOptions } from '@eggjs/tegg-orm-decorator';
+
+declare module '@eggjs/tegg-orm-decorator' {
+  export declare function Attribute(dataType: DataType, options?: AttributeOptions): (target: any, propertyKey: PropertyKey) => void;
+}
