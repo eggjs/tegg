76ef679d745deb235db9dcc3fa34984b511bd5c6
killa <killa123@126.com>
2023-02-14 11:54:09 +0800

    fix: get app/ctx properties when load unit beforeCreate (#102)

--- plugin/tegg/lib/EggQualifierProtoHook.ts
+++ plugin/tegg/lib/EggQualifierProtoHook.ts
@@ -17,17 +17,15 @@ import { ObjectUtils } from '@eggjs/tegg-common-util';
 
 export class EggQualifierProtoHook implements LifecycleHook<LoadUnitLifecycleContext, LoadUnit> {
   private readonly app: Application;
-  private readonly appProperties: string[];
-  private readonly ctxProperties: string[];
 
   constructor(app: Application) {
     this.app = app;
-    this.appProperties = ObjectUtils.getProperties(this.app);
-    this.ctxProperties = ObjectUtils.getProperties(this.app.context);
   }
 
   async preCreate(ctx: LoadUnitLifecycleContext): Promise<void> {
     const clazzList = ctx.loader.load();
+    const appProperties = ObjectUtils.getProperties(this.app);
+    const ctxProperties = ObjectUtils.getProperties(this.app.context);
     for (const clazz of clazzList) {
       for (const injectObject of PrototypeUtil.getInjectObjects(clazz) || []) {
         const propertyQualifiers = QualifierUtil.getProperQualifiers(clazz, injectObject.refName);
@@ -35,16 +33,16 @@ export class EggQualifierProtoHook implements LifecycleHook<LoadUnitLifecycleCon
         if (hasEggQualifier) {
           continue;
         }
-        if (this.isCtxObject(injectObject.objName)) {
+        if (this.isCtxObject(injectObject.objName, ctxProperties)) {
           QualifierUtil.addProperQualifier(clazz, injectObject.refName, EggQualifierAttribute, EggType.CONTEXT);
-        } else if (this.isAppObject(injectObject.objName)) {
+        } else if (this.isAppObject(injectObject.objName, appProperties)) {
           QualifierUtil.addProperQualifier(clazz, injectObject.refName, EggQualifierAttribute, EggType.APP);
         }
       }
     }
   }
 
-  private isAppObject(name: PropertyKey) {
+  private isAppObject(name: PropertyKey, appProperties: string[]) {
     name = String(name);
     if (APP_CLAZZ_BLACK_LIST.includes(name)) {
       return false;
@@ -52,10 +50,10 @@ export class EggQualifierProtoHook implements LifecycleHook<LoadUnitLifecycleCon
     if (DEFAULT_APP_CLAZZ.includes(name)) {
       return true;
     }
-    return this.appProperties.includes(name);
+    return appProperties.includes(name);
   }
 
-  private isCtxObject(name: PropertyKey) {
+  private isCtxObject(name: PropertyKey, ctxProperties: string[]) {
     name = String(name);
     if (CONTEXT_CLAZZ_BLACK_LIST.includes(name)) {
       return false;
@@ -63,6 +61,6 @@ export class EggQualifierProtoHook implements LifecycleHook<LoadUnitLifecycleCon
     if (DEFAULT_CONTEXT_CLAZZ.includes(name)) {
       return true;
     }
-    return this.ctxProperties.includes(name);
+    return ctxProperties.includes(name);
   }
 }
