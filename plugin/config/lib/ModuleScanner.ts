a4908c6c640000c7068def57d32052cca15adf47
killa <killa123@126.com>
2024-01-17 10:20:01 +0800

    feat: scan framework dependencies as optional module (#184)
    <!--
    Thank you for your pull request. Please review below requirements.
    Bug fixes and new features should include tests and possibly benchmarks.
    Contributors guide:
    https://github.com/eggjs/egg/blob/master/CONTRIBUTING.md
    
    感谢您贡献代码。请确认下列 checklist 的完成情况。
    Bug 修复和新功能必须包含测试，必要时请附上性能测试。
    Contributors guide:
    https://github.com/eggjs/egg/blob/master/CONTRIBUTING.md
    -->
    
    ##### Checklist
    <!-- Remove items that do not apply. For completed items, change [ ] to
    [x]. -->
    
    - [x] `npm test` passes
    - [x] tests and/or benchmarks are included
    - [ ] documentation is changed or added
    - [x] commit message follows commit guidelines
    
    ##### Affected core subsystem(s)
    <!-- Provide affected core subsystem(s). -->
    
    
    ##### Description of change
    <!-- Provide a description of the change below this comment. -->
    
    <!--
    - any feature?
    - close https://github.com/eggjs/egg/ISSUE_URL
    -->

---
+++ plugin/config/lib/ModuleScanner.ts
@@ -0,0 +1,41 @@
+import { ModuleConfigUtil, ModuleReference, ReadModuleReferenceOptions } from '@eggjs/tegg-common-util';
+import path from 'path';
+
+export class ModuleScanner {
+  private readonly baseDir: string;
+  private readonly readModuleOptions: ReadModuleReferenceOptions;
+
+  constructor(baseDir: string, readModuleOptions: ReadModuleReferenceOptions) {
+    this.baseDir = baseDir;
+    this.readModuleOptions = readModuleOptions;
+  }
+
+  /**
+   * - load module references from config or scan from baseDir
+   * - load framework module as optional module reference
+   */
+  loadModuleReferences(): readonly ModuleReference[] {
+    const moduleReferences = ModuleConfigUtil.readModuleReference(this.baseDir, this.readModuleOptions || {});
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
+    const appPkg = require(path.join(this.baseDir, 'package.json'));
+    const framework = appPkg.egg?.framework;
+    if (!framework) {
+      return moduleReferences;
+    }
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
+    const frameworkPkg = require.resolve(`${framework}/package.json`, {
+      paths: [ this.baseDir ],
+    });
+    const frameworkDir = path.dirname(frameworkPkg);
+    const optionalModuleReferences = ModuleConfigUtil.readModuleReference(frameworkDir, this.readModuleOptions || {});
+    return [
+      ...moduleReferences,
+      ...optionalModuleReferences.map(t => {
+        return {
+          ...t,
+          optional: true,
+        };
+      }),
+    ];
+  }
+}
