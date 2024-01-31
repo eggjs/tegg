d087e7c646faf50f1fed19a556a455c689d56ef2
Angela Wang <idu.angela@gmail.com>
2024-01-31 18:05:30 +0800

    feat: add getEggObjects API to fetch all instances

--- core/dynamic-inject/src/EggObjectFactory.ts
+++ core/dynamic-inject/src/EggObjectFactory.ts
@@ -1,6 +1,8 @@
-import { EggAbstractClazz } from './typing';
 import { QualifierValue } from '@eggjs/core-decorator';
+
+import { EggAbstractClazz } from './typing';
 
 export interface EggObjectFactory {
   getEggObject<T extends object>(abstractClazz: EggAbstractClazz<T>, qualifierValue: QualifierValue): Promise<T>;
+  getEggObjects<T extends object>(abstractClazz: EggAbstractClazz<T>): Promise<AsyncIterable<T>>;
 }
