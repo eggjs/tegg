10846b106655c7b17b7dc4e340f037142815cfeb
killagu <zhubin.gzb@antgroup.com>
2024-09-29 15:10:20 +0800

    fix: fix DataSourceQualifier

--- core/dal-decorator/src/decorator/DataSourceQualifier.ts
+++ core/dal-decorator/src/decorator/DataSourceQualifier.ts
@@ -1,10 +1,8 @@
 import { DataSourceQualifierAttribute } from '@eggjs/tegg-types';
-import type { EggProtoImplClass } from '@eggjs/tegg-types';
 import { QualifierUtil } from '@eggjs/core-decorator';
 
 export function DataSourceQualifier(dataSourceName: string) {
   return function(target: any, propertyKey: PropertyKey, parameterIndex?: number) {
-    QualifierUtil.addInjectQualifier(target.constructor as EggProtoImplClass,
-      propertyKey, parameterIndex, DataSourceQualifierAttribute, dataSourceName);
+    QualifierUtil.addInjectQualifier(target, propertyKey, parameterIndex, DataSourceQualifierAttribute, dataSourceName);
   };
 }
