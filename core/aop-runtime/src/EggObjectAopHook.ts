d169bb2fbbc86335315619866b4134a25296f552
killa <zhubin.gzb@antgroup.com>
2024-10-10 15:21:09 +0800

    fix: fix aop in constructor inject type (#247)
    <!--
    Thank you for your pull request. Please review below requirements.
    Bug fixes and new features should include tests and possibly benchmarks.
    Contributors guide:
    https://github.com/eggjs/egg/blob/master/CONTRIBUTING.md
    
    感谢您贡献代码。请确认下列 checklist 的完成情况。
    Bug 修复和新功能必须包含测试，必要时请附上性能测试。
    Contributors guide:
    https://github.com/eggjs/egg/blob/master/CONTRIBUTING.md
    -->
    
    ##### Checklist
    <!-- Remove items that do not apply. For completed items, change [ ] to
    [x]. -->
    
    - [ ] `npm test` passes
    - [ ] tests and/or benchmarks are included
    - [ ] documentation is changed or added
    - [ ] commit message follows commit guidelines
    
    ##### Affected core subsystem(s)
    <!-- Provide affected core subsystem(s). -->
    
    
    ##### Description of change
    <!-- Provide a description of the change below this comment. -->
    
    <!--
    - any feature?
    - close https://github.com/eggjs/egg/ISSUE_URL
    -->
    
    <!-- This is an auto-generated comment: release notes by coderabbit.ai
    -->
    
    ## Summary by CodeRabbit
    
    - **New Features**
    - Enhanced AOP functionality with new hooks and advice injection
    methods.
    - Introduced a new module for constructor injection in AOP, improving
    dependency management.
      
    - **Bug Fixes**
    - Improved sequence of operations in module loading to ensure proper
    graph building.
    
    - **Tests**
    - Added a new test suite for constructor injection, increasing test
    coverage for AOP functionality.
    
    - **Documentation**
    - New `package.json` file created for the "aop-module" to define module
    configuration.
    
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->

--- core/aop-runtime/src/EggObjectAopHook.ts
+++ core/aop-runtime/src/EggObjectAopHook.ts
@@ -1,13 +1,35 @@
-import { ASPECT_LIST } from '@eggjs/tegg-types';
+import { ASPECT_LIST, InjectType } from '@eggjs/tegg-types';
 import type { EggObject, EggObjectLifeCycleContext, LifecycleHook } from '@eggjs/tegg-types';
 import { Aspect } from '@eggjs/aop-decorator';
 import { AspectExecutor } from './AspectExecutor';
+import { PrototypeUtil } from '@eggjs/core-decorator';
+import assert from 'node:assert';
+import { EggContainerFactory } from '@eggjs/tegg-runtime';
 
 export class EggObjectAopHook implements LifecycleHook<EggObjectLifeCycleContext, EggObject> {
   private hijackMethods(obj: any, aspectList: Array<Aspect>) {
     for (const aspect of aspectList) {
       const newExecutor = new AspectExecutor(obj, aspect.method, aspect.adviceList);
       obj[aspect.method] = newExecutor.execute.bind(newExecutor);
+    }
+  }
+
+  // constructor inject only paas obj to constructor
+  // should manually define obj to property
+  private injectAdvice(eggObject: EggObject, obj: any, aspectList: Array<Aspect>) {
+    if (eggObject.proto.getMetaData(PrototypeUtil.INJECT_TYPE) !== InjectType.CONSTRUCTOR) {
+      return;
+    }
+    for (const aspect of aspectList) {
+      for (const advice of aspect.adviceList) {
+        const injectObject = eggObject.proto.injectObjects.find(t => t.objName === advice.name);
+        assert(injectObject, `not found inject advice ${advice.name}`);
+        const adviceObj = EggContainerFactory.getEggObject(injectObject!.proto, advice.name);
+        Object.defineProperty(obj, advice.name, {
+          value: adviceObj.obj,
+          enumerable: false,
+        });
+      }
     }
   }
 
@@ -25,12 +47,14 @@ export class EggObjectAopHook implements LifecycleHook<EggObjectLifeCycleContext
           if (!obj) {
             obj = Reflect.apply(propertyDesc.get!, eggObject, []);
             self.hijackMethods(obj, aspectList);
+            self.injectAdvice(eggObject, obj, aspectList);
           }
           return obj;
         },
       });
     } else {
       this.hijackMethods(eggObject.obj, aspectList);
+      this.injectAdvice(eggObject, eggObject.obj, aspectList);
     }
   }
 }
