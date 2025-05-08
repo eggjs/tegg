1bd8bb8bdf97dc17f57a81e22a2c8592118746f9
killagu <zhubin.gzb@antgroup.com>
2025-05-08 20:49:38 +0800

    f

--- core/dal-runtime/test/SqlUtil.test.ts
+++ core/dal-runtime/test/SqlUtil.test.ts
@@ -1,5 +1,5 @@
 import { SqlUtil } from '../src/SqlUtil';
-import assert from 'assert';
+import assert from 'node:assert/strict';
 
 it('should preserve SQL hint and remove normal comments', () => {
   const sql = 'SELECT /*+ INDEX(a) */ * FROM table /* this is a comment */ WHERE id = 1';
