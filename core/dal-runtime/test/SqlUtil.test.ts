1c0f6cc6285b1545034bf9fcfa27baaee5719df9
killagu <zhubin.gzb@antgroup.com>
2025-05-08 20:35:52 +0800

    f

--- core/dal-runtime/test/SqlUtil.test.ts
+++ core/dal-runtime/test/SqlUtil.test.ts
@@ -5,4 +5,4 @@ it('should preserve SQL hint and remove normal comments', () => {
   const sql = 'SELECT /*+ INDEX(a) */ * FROM table /* this is a comment */ WHERE id = 1';
   const result = SqlUtil.minify(sql);
   assert.strictEqual(result, 'SELECT /*+ INDEX(a) */ * FROM table WHERE id = 1');
-}); 
+});
