33069089a5a9c9d4ca244cacaa4eab95459f1dd7
akitaSummer <akitasummer@outlook.com>
2025-07-31 15:28:00 +0800

    fix: add assert

--- plugin/controller/test/fixtures/apps/mcp-app/config/config.default.js
+++ plugin/controller/test/fixtures/apps/mcp-app/config/config.default.js
@@ -5,9 +5,6 @@ const { randomUUID } = require('node:crypto');
 module.exports = function() {
   const config = {
     keys: 'test key',
-    security: {
-      csrf: false,
-    },
     mcp: {
       sessionIdGenerator: ctx => {
         return ctx.request.headers['custom-session-id'] || randomUUID();
