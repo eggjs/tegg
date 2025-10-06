5efffa8b2aedf52df450c2d48464b741104e7f48
MK <fengmk2@gmail.com>
2025-10-06 13:33:31 +0800

    FIXUP

--- core/controller-decorator/test/decorators.test.ts
+++ core/controller-decorator/test/decorators.test.ts
@@ -6,6 +6,8 @@ import { FooController } from './fixtures/HTTPFooController.js';
 describe('test/decorators.test.ts', () => {
   describe('', () => {
     it('should get the right file path', () => {
+      console.warn(FooController.fileName);
+      console.warn(PrototypeUtil.getFilePath(FooController));
       assert.equal(PrototypeUtil.getFilePath(FooController), FooController.fileName);
     });
   });
