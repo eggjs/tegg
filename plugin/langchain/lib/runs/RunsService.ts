95a01a35159694b9e15f39648470cc1e51a63be0
raohai <surgesoft@gmail.com>
2025-12-25 17:34:46 +0800

    feat: sse-utils and test case

--- plugin/langchain/lib/runs/RunsService.ts
+++ plugin/langchain/lib/runs/RunsService.ts
@@ -1,9 +1,8 @@
-import { AccessLevel, Inject, ModuleConfig, ModuleQualifier, SingletonProto } from '@eggjs/tegg';
+import { AccessLevel, Inject, ModuleConfig, SingletonProto } from '@eggjs/tegg';
 
 @SingletonProto({ accessLevel: AccessLevel.PUBLIC })
 export class RunsService {
   @Inject()
-  @ModuleQualifier('')
   private readonly moduleConfig: ModuleConfig;
 
   public getConfigs() {
