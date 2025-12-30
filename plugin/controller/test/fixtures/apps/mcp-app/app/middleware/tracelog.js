be310f272f595d925fae89ab74f7cf45af0f7d86
akitaSummer <akitasummer@outlook.com>
2025-12-30 14:16:23 +0800

    fix: ci

--- plugin/controller/test/fixtures/apps/mcp-app/app/middleware/tracelog.js
+++ plugin/controller/test/fixtures/apps/mcp-app/app/middleware/tracelog.js
@@ -3,6 +3,8 @@
 module.exports = () => {
   return async function tracelog(ctx, next) {
     ctx.req.headers.trace = 'middleware';
+    ctx.req.rawHeaders.push('trace');
+    ctx.req.rawHeaders.push('middleware');
     await next();
   };
 };
